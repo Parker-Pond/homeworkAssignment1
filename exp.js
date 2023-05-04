2d57d3c327abe796baedca1e0f01acc71eb5d736
Work <Work@Parkers-MacBook-Air.local>
2023-05-04 12:15:19 -0600

    first commit

---
+++ exp.js
@@ -0,0 +1,14 @@
+// creqte three math problems which result in 10, 40 39
+// create a string which cocatinate tge strubgs ubti a mnessafge 
+// message says that they you where randomly selected to have the access codes to a secret vault...
+
+// safe code math problems
+const safeCode1 = (12 * 10)/12 
+const safeCode2 = (28 +12)
+const safeCode3 = (49 - 10)
+
+// pop up dialof box messafe
+window.alert(`You have been randomly selected to recieve the safe code which is not open to the public the Numbers are 
+${ safeCode1} - ${safeCode2} -  ${safeCode3} 
+may the odds ever be in your favor.` )
+
